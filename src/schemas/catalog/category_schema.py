2d4bf15120cad1503db2865c8cb8eff02fc8b247
lucas.pessoa <lucas.pessoa@embeddo.io>
2026-07-04 22:22:40 -0300

    doc: remove comments

--- src/schemas/catalog/category_schema.py
+++ src/schemas/catalog/category_schema.py
@@ -12,12 +12,9 @@ class CategoryCreateSchema(CategoryBaseSchema):
     pass
 
 class CategoryUpdateSchema(CategoryBaseSchema):
-    pass # O ID vai na URL, não precisamos dele no corpo do Update
-
+    pass 
 class CategoryResponseSchema(CategoryBaseSchema):
     id: UUID
     created_at: datetime
-    # deleted_at geralmente não é devolvido para o front-end
-
-    # Sintaxe correta do Pydantic V2 para converter Models SQLAlchemy em JSON
+   
     model_config = ConfigDict(from_attributes=True)
